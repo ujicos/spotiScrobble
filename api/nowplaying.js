b6b39e416ddac586b96ebd90b9184a3cc83d658a
uji <94710096+ujicos@users.noreply.github.com>
2025-09-17 23:26:32 +0200

    oops

--- api/nowplaying.js
+++ api/nowplaying.js
@@ -1,29 +1,20 @@
-import axios from 'axios';
-import fs from 'fs';
-
+// /api/nowplaying.js
 export default async function handler(req, res) {
   try {
-    const tokens = JSON.parse(fs.readFileSync('tokens.json'));
-    const access_token = tokens.access_token;
+    // 🔹 Midlertidig testdata
+    // Du kan erstatte med ekte Spotify API-kall fra Shortcut senere
+    const track = "Billie Jean";
+    const artist = "Michael Jackson";
+    const album = "Thriller";
 
-    const response = await axios.get('https://api.spotify.com/v1/me/player/currently-playing', {
-      headers: {
-        Authorization: `Bearer ${access_token}`
-      }
+    // 🔹 Returner JSON til Shortcut
+    res.status(200).json({
+      track: track,
+      artist: artist,
+      album: album
     });
-
-    if (response.status === 204 || !response.data) {
-      return res.json({playing: false});
-    }
-
-    const track = response.data.item;
-    const logLine = `${Math.floor(Date.now()/1000)}\t${track.artists.map(a=>a.name).join(', ')}\t${track.name}\t${track.album.name}\n`;
-    
-    // Lagre til scrobble.log
-    fs.appendFileSync('scrobble.log', logLine);
-
-    res.json({playing: true, track: track.name, artist: track.artists.map(a=>a.name).join(', '), album: track.album.name});
-  } catch(err) {
-    res.status(500).json({error: err.message});
+  } catch (err) {
+    console.error("Error in nowplaying:", err);
+    res.status(500).json({ error: "Internal Server Error" });
   }
 }
